0f260da830e8295b011436b68ede5c5cb61f68e9
njaremko <njaremko@gmail.com>
2016-04-07 13:54:18 -0400

    Much improved handling of tip percent specification

--- tipCalc.go
+++ tipCalc.go
@@ -2,6 +2,7 @@ package tip
 
 import (
 	"github.com/itsabot/abot/shared/datatypes"
+	"github.com/itsabot/abot/shared/language"
 	"github.com/itsabot/abot/shared/nlp"
 	"github.com/itsabot/abot/shared/plugin"
 	"log"
@@ -47,26 +48,37 @@ func FollowUp(in *dt.Msg) (string, error) {
 }
 
 func parseTip(in *dt.Msg) string {
+	// Amount of money spent
 	var amount float64 = 0
+	// Default to a 15% tip to be safe
 	var tip float64 = 15
+	// Input sentence separated by
+	var tokenizedSentence []string = in.Tokens
 
-	for _, obj := range in.Tokens {
-		if strings.Contains(obj, "$") {
-			amount, _ = strconv.ParseFloat(strings.TrimPrefix(obj, "$"), 64)
-		}
-
-		if strings.Contains(obj, "%") {
-			tip, _ = strconv.ParseFloat(strings.TrimSuffix(obj, "%"), 64)
+	for i := 0; i < len(tokenizedSentence); i++ {
+		// Handle specified percentage for tips
+		if strings.Contains(tokenizedSentence[i], "%") {
+			if tempTip, err := strconv.ParseFloat(strings.TrimSuffix(tokenizedSentence[i], "%"), 64); err == nil {
+				tip = tempTip
+			}
+		} else if i < len(tokenizedSentence)-1 && tokenizedSentence[i+1] == "percent" {
+			if tempTip, err := strconv.ParseFloat(tokenizedSentence[i], 64); err == nil {
+				tip = tempTip
+			}
+			// If the previous two cases aren't true, but this is. Then it should be the amount spent.
+		} else if temp := language.ExtractCurrency(tokenizedSentence[i]); temp.Valid == true {
+			amount = float64(temp.Int64)
 		}
 	}
+	// Return the final string
 	if amount != 0 {
 		return "I recommend you tip $" + calcTip(amount, tip)
 	} else {
-		return "Please make sure you include the '$' symbol prefix."
+		return "I'm sorry, but you didn't specify an amount of money"
 	}
 }
 
 func calcTip(spent float64, tip float64) string {
-	result := spent * tip / 100
+	result := spent * tip / 10000
 	return strconv.FormatFloat(result, 'f', 2, 64)
 }
